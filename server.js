489224ff718793090fb142a0a9f3cac312ba67c7
joel cronin <you@example.com>
2022-07-11 16:44:56 -0400

    all thoughts and user/thoughts by Id routes working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const db = require('./config/connection');
 // Require model
 const { User, Thought } = require('./models');
 
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,9 +11,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Get all Users
-
-app.get('/all-users', (req, res) => {
+app.get('/api/users', (req, res) => {
   User.find({}, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+});
+
+//Get one User by ID
+app.get('/api/users/:id', (req, res) => {
+  User.findOne({_id: req.params.id}, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+});
+
+//Get all Thoughts
+app.get('/api/thoughts', (req, res) => {
+  Thought.find({}, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+});
+
+//Get one Thought by ID
+app.get('/api/thoughts/:id', (req, res) => {
+  Thought.findOne({_id: req.params.id}, (err, result) => {
     if (result) {
       res.status(200).json(result);
     } else {
