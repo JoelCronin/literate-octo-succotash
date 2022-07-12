cb1aafe9501dd09b30a869e00eb57e33288a3527
joel cronin <you@example.com>
2022-07-12 18:58:31 -0400

    all User routes working as intended

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,6 +6,8 @@ const {
     createUser,
     updateUser,
     deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/userController')
 
 // /api/users
@@ -16,5 +18,7 @@ router.route('/:userId')
 .get(getSingleUser)
 .put(updateUser)
 .delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').put(addFriend).delete(deleteFriend)
 
 module.exports = router;
