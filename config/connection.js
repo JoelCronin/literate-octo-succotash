55e6b219d3d59f0c3600051b55e11e882d6de886
joel cronin <you@example.com>
2022-07-12 16:30:16 -0400

    switched to using controllers, user routes set up and working

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
 
-// Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/socialMediaDB', {
+
+const { connect, connection } = require('mongoose');
+
+connect('mongodb://localhost/socialMediaDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Export connection 
-module.exports = mongoose.connection;
+module.exports = connection;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getSingleUser,
+    createUser,
+} = require('../../controllers/userController')
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser);
+
+module.exports = router;
